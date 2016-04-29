4f1e1a00215779b00d330b8be226e0075fea8b91
jsalor <jorgesalor24@gmail.com>
2016-04-29 10:25:25 +0200

    ika

---
+++ StringArrayUtilsTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringArrayUtilsTest {
+	
+	StringArrayUtils Array1 = new StringArrayUtils;
+
+	@Test
+	public void testStringArrayUtils() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaxLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTotalLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIndexOf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
