1db43dc5bdf7bd1b8965f366bdb354e936ae6f97
dieescal <alexx.dediego@gmail.com>
2016-05-05 09:34:16 +0200

    Añadiendo archivo Aplicacion.java para pruebas
    Signed-off-by: dieescal <alexx.dediego@gmail.com>

---
+++ Ejem08_CarritoCompra/Aplicacion.java
@@ -0,0 +1,13 @@
+
+public class Aplicacion {
+
+	public static void main(String[] args){
+		
+		Product a=new Product("Teclado logitec",15);
+		Product b=new Product("Raton logitec",15);
+		Product c=new Product("Monitor Samsung",15);
+		Product d=new Product("Teclado",15);
+		
+		
+	}
+}
